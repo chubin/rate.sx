ea733771c6d4ca2166ba6ca63cc573d102347150
Igor Chubin <igor@chub.in>
2018-01-14 17:55:03 +0000

    minor fixes in lib/spark.py

--- lib/spark.py
+++ lib/spark.py
@@ -4,31 +4,39 @@ import math
 from termcolor import colored
 
 def my_ceil(v):
-
+    if v < 0:
+        if math.ceil(v) != v:
+            v -= 1
     result = math.ceil(v)
-    if result < 0:
-        result -= 1
-    if result == 0:
-        result = 1
     return int(result)
 
 def to_bar(x):
-    bars = u'▁▂▃▅▇'
+    bars = u'_▁▂▃▅▇'
     clr = 'green'
     if x < 0:
         clr = 'red'
         x = -x
-    return colored(bars[(x-1)%len(bars)], clr)
+    return colored(bars[x%len(bars)], clr)
 
+def spark_numbers(vals):
+    deltas = [y-x for (x,y) in zip(vals[:-1], vals[1:])]
+    max_delta = max(abs(x) for x in deltas)
+
+    if max_delta == 0:
+        mapped_deltas = [ 0 for x in deltas ]
+    else:
+        mapped_deltas = [ my_ceil(x*5.0/max_delta) for x in deltas ]
+
+    return mapped_deltas
 
 def spark(vals):
-    deltas = [ y-x for (x,y) in zip(vals[:-1], vals[1:]) ]
-
-
-    max_delta = max(abs(x) for x in deltas)
-    mapped_deltas = [ my_ceil(x*5.0/max_delta) for x in deltas ]
-
+    mapped_deltas = spark_numbers(vals)
     bars = [to_bar(x) for x in mapped_deltas]
 
-    return "".join(bars).encode('utf-8')
+    return "".join(bars)
+
+if __name__ == '__main__':
+    data = [1,-1,2,-2,3,-3,4,-4,5,-5]
+    print spark_numbers(data)
+    print spark(data)
 
